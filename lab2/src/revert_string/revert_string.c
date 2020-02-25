24af7dde52f5ee1a0b49889b3d4a2262a6b20712
daria-karam <tirgi.mail@gmail.com>
2020-02-25 10:13:23 +0000

    дописан коÐadded code for the function Revert_string

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -2,6 +2,20 @@
 
 void RevertString(char *str)
 {
-	// your code here
+	int n = 0;
+    int i = 0;
+    for (i = 0; str[i]!= 0; i++)
+    {
+        n++;
+        //printf("%c\n", str[i]);
+    }
+    //printf("n: %d\n", n);
+    for (i = 0; i < (n+1)/2; i++)
+    {
+        //printf("%c %c\n", str[i], str[n-i-1]);
+        char between = str[i];
+        str[i] = str[n-i-1];
+        str[n-i-1] = between;
+    }
 }
 
