765d54dc75c21b27d9ff7fc16ff7b420a8c9eeda
daria-karam <tirgi.mail@gmail.com>
2020-03-07 08:45:20 +0000

    all done

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -7,15 +7,11 @@ void RevertString(char *str)
     for (i = 0; str[i]!= 0; i++)
     {
         n++;
-        //printf("%c\n", str[i]);
     }
-    //printf("n: %d\n", n);
     for (i = 0; i < n/2; i++)
     {
-        //printf("%c %c\n", str[i], str[n-i-1]);
         char between = str[i];
         str[i] = str[n-i-1];
         str[n-i-1] = between;
     }
 }
-
