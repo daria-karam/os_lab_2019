bd9292fd73104a59282fa59c739597368e7f6f01
daria-karam <tirgi.mail@gmail.com>
2020-04-10 19:38:19 +0000

    task5 add

---
+++ lab3/src/task5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+int main(int argc, char **argv)
+{
+    pid_t child_pid = fork();
+    if(child_pid == 0){
+        execl("sequential_min_max", " ", argv[1], argv[2], NULL);
+    }
+    else
+        return 0;
+}
